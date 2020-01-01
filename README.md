b8d430af724441a1dfdbc8d6da11c22862d748a6
endormi <ernosalo14@gmail.com>
2020-01-01 13:47:42 +0200

    Add two GET methods and mock data

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Go-API
 
-Simple API for Go using Mux.
+Simple API for Go using Mux with mock data.
+
+You might need to get mux:
+
+```sh
+go get github.com/gorilla/mux
+```
+
+Run:
+
+```sh
+go run main.go
+```
--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Language struct {
+	ID string `json:"id,omitempty"`
+	Creator string `json:"creator"`
+	Name string `json:"name"`
+	Paradigm string `json:"paradigm"`
+}
+
+var Lang []Language
+
+func GetLanguage(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range Lang {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Language{})
+}
+
+func GetLanguages(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(Lang)
+}
+
 func main() {
 	router := mux.NewRouter()
+	Lang = append(Lang, Language{ID: "1", Creator: "Guido Van Rossum", Name: "Python", Paradigm: "Multi-paradigm: functional, imperative, object-oriented, reflective"})
+	Lang = append(Lang, Language{ID: "2", Creator: "Brendan Eich", Name: "JavaScript", Paradigm: "Multi-paradigm: event-driven, functional, imperative, object-oriented (prototype-based)"})
+	router.HandleFunc("/language", GetLanguages).Methods("GET")
+	router.HandleFunc("/language/{id}", GetLanguage).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
